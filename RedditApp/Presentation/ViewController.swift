9431ae026444a9aa61ded0962c37e15c841fb9c1
Manuel Munoz <manuelmunozr.92@gmail.com>
2020-03-04 23:43:53 -0300

    Added some validations when receiving redirectURI

--- RedditApp/Presentation/ViewController.swift
+++ RedditApp/Presentation/ViewController.swift
@@ -49,6 +49,11 @@ extension ViewController: WKNavigationDelegate {
                 return
         }
         
-        query.split("&")
+        let components = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        if let stateValue = components?.queryItems?.first(where: { $0.name == "state" })?.value,
+        stateValue == oauthHandler.state {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            decisionHandler(.cancel)
+        }
     }
 }
